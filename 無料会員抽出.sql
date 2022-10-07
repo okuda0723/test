e076e2e14c398231d068058de1a1ee0c40928dcd
okuda0723 <92144076+okuda0723@users.noreply.github.com>
2022-10-07 16:58:24 +0900

    Update 無料会員抽出.sql

--- 無料会員抽出.sql
+++ 無料会員抽出.sql
@@ -1,11 +1,10 @@
-SELECT
-'"' || u.email || '"' || ',', COALESCE(p.type, '無料会員')
+SELECT '"' || u.email || '"' || ',', COALESCE(p.type, '無料会員')
 FROM public."User" AS u
-LEFT JOIN public."Billing" AS  b
-ON (u.id = b.user_id)
-LEFT JOIN public."BillingPlan" AS p
-    ON (b.billing_plan_id = p.id)
-LEFT JOIN public."_MailMagazineToUser" AS m
-ON (u.id = m."B")
+    LEFT JOIN public."Billing" AS  b
+        ON (u.id = b.user_id)
+    LEFT JOIN public."BillingPlan" AS p
+        ON (b.billing_plan_id = p.id)
+    LEFT JOIN public."_MailMagazineToUser" AS m
+        ON (u.id = m."B")
 WHERE b.user_id IS NULL
-AND m."B" IS NOT NULL; --無料会員全体送信の際はコメントアウトする
+  AND m."B" IS NOT NULL; --無料会員全体送信の際はコメントアウトする
